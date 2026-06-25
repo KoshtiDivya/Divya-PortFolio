abd7fb61b86367e80f18b33e80e8a5262b2df098
Divya Koshti <koshtidivya2004@gmail.com>
2026-06-25 19:41:47 +0530

    Fix resume download path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import profile from "../assets/divya profile.png";
-const role = ['“Frontend Developer”', '“Full Stack (MERN) Developer”', '"React Developer"'];
+const role = [
+  "“Frontend Developer”",
+  "“Full Stack (MERN) Developer”",
+  '"React Developer"',
+];
 
 const Home = () => {
   const [text, setText] = useState("");
@@ -9,40 +13,62 @@ const Home = () => {
 
   useEffect(() => {
     const current = role[index];
-    const timeout = setTimeout(() => {
-      if (!isDeleting) {
-        setText(current.substring(0, text.length + 1));
+    const timeout = setTimeout(
+      () => {
+        if (!isDeleting) {
+          setText(current.substring(0, text.length + 1));
 
-        if (text == current) {
-          setTimeout(() => setIsDeleting(true), 1000);
+          if (text == current) {
+            setTimeout(() => setIsDeleting(true), 1000);
+          }
+        } else {
+          setText(current.substring(0, text.length - 1));
+          if (text == "") {
+            setIsDeleting(false);
+            setIndex((prev) => (prev + 1) % role.length);
+          }
         }
-      } else {
-        setText(current.substring(0, text.length - 1));
-        if (text == "") {
-          setIsDeleting(false);
-          setIndex((prev) => (prev + 1) % role.length);
-        }
-      }
-    }, isDeleting ? 50 : 100);
+      },
+      isDeleting ? 50 : 100,
+    );
     return () => clearTimeout(timeout);
   }, [text, isDeleting, index]);
   return (
     <section id="intro">
-      <div className="introContent" data-aos="fade-up-right" data-aos-duration="1000">
+      <div
+        className="introContent"
+        data-aos="fade-up-right"
+        data-aos-duration="1000"
+      >
         <span className="hello">Hello, </span> <br />
         <span>
           I'm <span className="introName">Divya</span> <br />
           <span className="role">{text}</span>
           <span className="role">|</span>
         </span>
-        <p className="introPara">I build modern, responsive, and user-friendly web applications using React, Node.js, and MongoDB.  With a strong foundation in frontend development and growing backend expertise, I enjoy turning ideas into real-world applications.</p>
-        <p className="introPara">Currently open to internships and freelance opportunities.</p>
-        <a href="public\Divya_Koshti_Resume.pdf" download><button className="btn"><i className="fa-solid fa-download" />Download Resume</button></a>
+        <p className="introPara">
+          I build modern, responsive, and user-friendly web applications using
+          React, Node.js, and MongoDB. With a strong foundation in frontend
+          development and growing backend expertise, I enjoy turning ideas into
+          real-world applications.
+        </p>
+        <p className="introPara">
+          Currently open to internships and freelance opportunities.
+        </p>
+        <a href="/Divya_Koshti_Resume.pdf" download>
+          <button className="btn">
+            <i className="fa-solid fa-download"></i>
+            Download Resume
+          </button>
+        </a>
       </div>
-      <div className="profile-img" data-aos="fade-up-left" data-aos-duration="1000">
-          <img src={profile} alt="profile"/>
+      <div
+        className="profile-img"
+        data-aos="fade-up-left"
+        data-aos-duration="1000"
+      >
+        <img src={profile} alt="profile" />
       </div>
-     
     </section>
   );
 };
